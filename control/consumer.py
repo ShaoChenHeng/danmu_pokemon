991f9623520f5200e221a066634c3e47f12c3a81
scheng <scheng52123@gmail.com>
2022-03-24 23:01:12 +0800

    init reposity

---
+++ control/consumer.py
@@ -0,0 +1,61 @@
+import pyautogui
+import redis
+import time
+
+loop_sec = 0.2
+press_sec = 0.5
+list_name = 'bilibili'
+key_list = ('w', 's', 'a', 'd', 'j', 'k', 'u', 'i', 'z', 'x', 'f', 'g','h')
+
+
+def init_redis():
+    r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+    return r
+
+
+def control(key_name):
+    
+    if key_name is None:
+        return
+    
+    key_name = key_name.lower()
+
+    if key_name in key_list:
+        print("发出指令", key_name)
+        pyautogui.keyDown(key_name)
+        
+        # if key_name == 'a' or key_name == 'd':
+        #     print("发出指令w")
+        #     pyautogui.keyDown('w')
+
+        
+        pyautogui.keyUp(key_name)
+        # if key_name == 'a' or key_name == 'd':
+        #     print("发出指令w")
+        #     pyautogui.keyUp('w')
+
+
+def rush(key_name):
+    persist_time = int(key_name[2])
+    pyautogui.keyDown('j')
+    pyautogui.keyDown(key_name[1])
+
+    time.sleep(persist_time * 0.3)
+                                
+    pyautogui.keyUp(key_name[1])
+    pyautogui.keyUp('j')
+    
+
+
+if __name__ == '__main__':
+    r = init_redis()
+    print("开始监听弹幕消息, loop_sec =", loop_sec)
+    while True:
+        key_name = r.lpop(list_name)
+        if key_name is None:
+            continue
+        if len(key_name) == 3:
+            rush(key_name)
+        else:
+            control(key_name)
+        time.sleep(loop_sec)
---
+++ danmu/main.py
@@ -0,0 +1,65 @@
+# 部分弹幕功能代码来自项目：https://github.com/IsoaSFlus/danmaku，感谢大佬
+# 快手弹幕代码来源及思路：https://github.com/py-wuhao/ks_barrage，感谢大佬
+# 仅抓取用户弹幕，不包括入场提醒、礼物赠送等。
+
+import asyncio
+import danmaku
+import redis
+import pyautogui
+
+list_name = 'bilibili'
+key_list = ('w', 's', 'a', 'd', 'j', 'k', 'u', 'i', 'z', 'x', 'f', 'i', 'o','g','h', 'l')
+direction = ('w', 's', 'a', 'd')
+
+def init_redis():
+    r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+    return r
+
+def direction_number(list_str):
+    temp = []
+    if len(list_str) >= 2 and '1' <= list_str[1] \
+       and list_str[1] <= '9' and \
+       list_str[0] in key_list:
+        # temp.append(list_str[0])
+        for i in range(3 * int(list_str[1])):
+            temp.append(list_str[0])
+        return temp
+    return list_str
+    
+async def printer(q, redis):
+    while True:
+        m = await q.get()
+        if m['msg_type'] == 'danmaku':
+            print(f'{m["name"]}：{m["content"]}')
+
+
+            list_str = list(m["content"])
+            if len(list_str) == 3 and list_str[0] == 'l' and\
+               list_str[1] in direction \
+               and '1' <= list_str[2] and list_str[2] <= '9':
+                
+                new_str = list_str[0] + list_str[1] + list_str[2]
+                redis.rpush(list_name, new_str)
+                
+            else:
+                print("弹幕拆分:", list_str)
+                list_str = direction_number(list_str)
+                print(list_str)
+            
+                for char in list_str:
+                    if char.lower() in key_list:
+                        print('推送队列：', char.lower())
+                        redis.rpush(list_name, char.lower())
+            
+
+async def main(url):
+    redis = init_redis()
+    q = asyncio.Queue()
+    dmc = danmaku.DanmakuClient(url, q)
+    asyncio.create_task(printer(q, redis))
+    await dmc.start()
+
+
+a = 'http://live.bilibili.com/24616287'
+asyncio.run(main(a))
+
